6568054f19c74cbd7569f88e18fd65ed591aca4f
purupup <Leshapyataev98@gmail.com>
2025-09-23 17:02:31 +0400

    fix: db model

--- db/database.py
+++ db/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
+from sqlalchemy import Integer, String, ForeignKey, Text, DateTime, UUID
+from sqlalchemy import func
+from datetime import datetime
 from sqlalchemy.orm import mapped_column
 from dotenv import load_dotenv
 from os import getenv
@@ -15,7 +18,8 @@ DB_HOST = getenv('DB_HOST')
 
 
 engine = create_engine(
-    f'postgresql://{DB_NAME}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+    f'postgresql+psycopg2://{DB_NAME}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}',
+    echo = True,
     )
 
 
@@ -23,8 +27,25 @@ class Base(DeclarativeBase):
     pass
 
 
-class User(Base):
-    __tablename__ = "user"
+class Question(Base):
+    __tablename__ = "question"
+    id = mapped_column(Integer, primary_key=True)
+    text: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[datetime] = mapped_column(insert_default=func.now())
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    
+    def __repr__(self) -> str:
+        return f'Question(id={self.id!r}, text={self.text!r}, created_at={self.created_at!r})'
+
+
+class Answer(Base):
+    __tablename__ = "answer"
+    id = mapped_column(Integer, primary_key=True)
+    question_id: Mapped[int] = mapped_column(ForeignKey("Question.id"))
+    user_id: Mapped[str] = mapped_column(UUID)
+    text: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[datetime] = mapped_column(insert_default=func.now())
+
+    def __repr__(self) -> str:
+        return f'Answer(id={self.id!r}, question_id={self.question_id!r},' \
+            f'user_id={self.user_id!r}, text={self.text!r},' \
+                 f' created_at={self.created_at!r})'
